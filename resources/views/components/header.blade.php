79ad57c4abe2aab3ac82452485c5ec3b0cc68744
gian paolo tocchi <tu@esempio.com>
2024-03-11 10:56:03 +0100

    user story 1 completata

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-    <div class="row">
+    <div class="row bg-danger">
         <div class="col-12">
             <h1>{{$title ?? "manca il titolo nella header"}}</h1>
         </div>
